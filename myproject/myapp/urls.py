2d36f8e082e2841bcf919ad9e0b49c999fe983ca
Pakshal <pakshal94@gmail.com>
2016-02-21 12:26:12 +0530

    first commit

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('myapp.views',
+    url(r'^$', 'list', name='list'),
+    url(r'^list/$', 'list', name='list'),
+)
+
---
+++ myproject/myapp/views.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from django.http import HttpResponseRedirect
+from django.core.urlresolvers import reverse
+
+from myapp.models import Document
+from myapp.forms import DocumentForm
+
+def list(request):
+    # Handle file upload
+    if request.method == 'POST':
+        form = DocumentForm(request.POST, request.FILES)
+        if form.is_valid():
+            newdoc = Document(docfile = request.FILES['docfile'])
+            newdoc.save()
+
+            # Redirect to the document list after POST
+            return HttpResponseRedirect(reverse('list'))
+    else:
+        form = DocumentForm() # A empty, unbound form
+
+    # Load documents for the list page
+    documents = Document.objects.last()
+
+    if(request.GET.get('mybtn')):
+        pass
+        
+    # Render list page with the documents and the form
+    return render_to_response(
+        'myapp/list.html',
+        {'documents': documents, 'form': form},
+        context_instance=RequestContext(request)
+    )
+
+def index(request):
+    return render_to_response('myapp/index.html')
